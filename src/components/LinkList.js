32ec77e26b7b72ceb9bbb66413a1ea9504bc60dc
ISalikhzyanov <eazy591998@gmail.com>
2022-09-05 20:29:29 +0300

    link copy function

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -25,7 +25,7 @@ const LinkList = ({links}) => {
                             <td className='target'>{link.target}</td>
                             <td>
                                 <a href={`http://79.143.31.216/s/${link.short}`}
-                                onClick={()=>{navigator.clipboard.writeText(`http://79.143.31.216/s/${link.short}`);window.alert('Текст скопирован')}}
+                                onClick={()=>{navigator.clipboard.writeText(`http://79.143.31.216/s/${link.short}`);window.alert('Ссылка скопирована')}}
                                 >{link.short} </a>
                             </td>
                             <td>{link.counter}</td>
